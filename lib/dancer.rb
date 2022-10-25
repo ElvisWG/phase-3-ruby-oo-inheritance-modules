b41c7beb3ee1b43a64175e86c3110c8631c1776b
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-10-25 21:39:52 +0300

    Updated

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,14 +1,13 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
-
-    class Dancer
+class Dancer
         include Dance
-        extend MetaDancing
-      
+
         attr_accessor :name
-      
+
         def initialize(name)
           @name = name
         end
+        extend MetaDancing
 
 end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,16 +1,18 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
 
+require_relative './dance_module'
+require_relative './meta_dancing_module'
 class Kid
-  include Dance
+    include Dance
 
-  attr_accessor :name
+    attr_accessor :name
 
-  def initialize(name)
-    @name = name
-  end
-  extend MetaDancing
+    def initialize(name)
+      @name = name
+    end
+    extend MetaDancing
 
-  
-  
+
+
 end
